cf1ad73e90cd34d46d7be9b952f4d86d82d39a4b
Mandeep Sidhu <mandeep.sidhu2@gmail.com>
2019-08-06 23:21:11 +0530

    added api versioning

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,12 @@
+module Api
+	module V1
+		class SessionsController < ApplicationController
+			before_action :set_api_current_user
+			def index
+				return render json:@api_current_user
+					
+					
+			end
+		end
+	end
+end
--- app/controllers/sessions_controller.rb
+++
@@ -1,8 +0,0 @@
-class SessionsController < ApplicationController
-	before_action :set_api_current_user
-	def index
-		return render json:@api_current_user
-			
-			
-	end
-end
